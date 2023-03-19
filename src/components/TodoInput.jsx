f891d607b7eb08a7ceb3e088e4dcec3ae7a21865
oguzhanascier <oguzhanascier20@gmail.com>
2023-03-19 21:01:11 +0300

    todo input methods

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react'
+import { MainContext, useContext } from '../context/context'
+
+function TodoInput() {
+    const { todoList, setTodoList, newValue, setNewValue } = useContext(MainContext)
+
+    useEffect(() => {
+      
+    }, [todoList])
+
+    const addItem = () => {
+       const item=  {
+        id: Math.floor(Math.random()*9999999),
+        value:newValue
+       }
+
+       if(!newValue){
+        console.log('Boş geçildi')
+       }else{
+        setTodoList(old => [...old, item])
+        setNewValue('')
+       }
+
+       console.log(todoList)
+    }
+
+   
+    
+
+    console.log(newValue)
+    return (
+        <div className='formGroup'>
+        <input className='todoInput' type="text" placeholder='what do you think' value={newValue} onChange={(e) => setNewValue(e.target.value)} />
+        <button className='addButton' onClick={addItem}>Add</button>
+        </div>
+    )
+}
+
+export default TodoInput
